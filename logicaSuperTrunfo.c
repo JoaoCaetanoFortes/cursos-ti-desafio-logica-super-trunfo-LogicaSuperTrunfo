82bd1289cf54f4f6dd6fd4dce5ad477ad3931598
JoaoCaetanoFortes <Joaocfortes@outlook.com>
2025-09-02 16:01:49 +0000

    Finalizado desafio 1 da materia 2

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,6 +8,8 @@ int main() {
     int popula, turismo;
     float pib, area;
     float densidade, pibcap;
+    float superpoder1;
+    int pontos = 0;
 
     //Variáveis usadas na segunda carta
     char letra2;
@@ -15,6 +17,8 @@ int main() {
     int popula2, turismo2;
     float pib2, area2;
     float densidade2, pibcap2;
+    float superpoder2;
+    int pontos2 = 0;
 
     // Perguntas/scans da primeira carta
     printf("\n\n\nOla, bem vindo ao super trunfo!\nDigite as informacoes da primeira carta:\n");
@@ -90,9 +94,81 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB: %.2f\n", pib2);
     printf("PIB por capita: %.2f\n", pibcap2);
-    printf("Pontos turisticos: %d\n\n\n\n", turismo2);
-     
-    return 0;
+    printf("Pontos turisticos: %d\n\n", turismo2);
+    
+    
 
-    //Eu melhorei o ultimo codigo para esse
+    //A partir daqui será utilizado o conhecimento do nível mestre para poder comparar cada valor:
+    
+
+    /*Eu fiz a comparação na prórpia impressão pra não precisar fazer uma variável para cada característica
+    de cada cidade, a única variável que precisarei usar a mais será a de supeor poder, justamente
+    pra linha de código de printf n ficar maior que ela deveria ser!*/
+
+    superpoder1 = (float) popula + (float) turismo + pib + area + densidade + pibcap;
+    superpoder2 = (float) popula2 + (float) turismo2 + pib2 + area2 + densidade2 + pibcap2;
+
+    printf("Batalha de cartas (Considere 0 para falso e 1 para verdadeiro):\n\n");
+    printf("A carta de código %s possui maior População que a carta de codigo %s? %d\n", codigo, codigo2, popula > popula2);
+    printf("A carta de código %s possui maior Área que a carta de codigo %s? %d\n", codigo, codigo2, area > area2);
+    printf("A carta de código %s possui maior PIB que a carta de codigo %s? %d\n", codigo, codigo2, pib > pib2);
+    printf("A carta de código %s possui maior Pontos Turísticos que a carta de codigo %s? %d\n", codigo, codigo2, turismo > turismo2);
+    printf("A carta de código %s possui melhor Densidade Populacional que a carta de codigo %s? %d\n", codigo, codigo2, densidade < densidade2);
+    printf("A carta de código %s possui maior PIB per Capita que a carta de codigo %s? %d\n\n", codigo, codigo2, pibcap > pibcap2);
+    printf("Para finalizar a batalha, vamos comparar os Super Poder delas, que consiste na soma de todos as Características\n");
+
+    //Aqui vou colocar uma impressão do Super Poder de cada carta pra termos essa comparação visual!
+    printf("Super Poder da primeira carta: %.2f\n", superpoder1);
+    printf("Super Poder da segunda carta: %.2f\n\n", superpoder2);
+    
+    //A partir daqui será usado os conhecimentos de estruturas de decisões:
+    printf("Comparação de cartas:\n\n");
+    printf("Atributo população: \nCarta 1: %d VS %d Carta 2\n\n", popula, popula2);
+    printf("Super Poder (todos os atributos):\nCarta 1 %.2f VS %.2f Carta 2\n\n", superpoder1, superpoder2);
+    printf("Atributo PIB: \nCarta 1: %.2f VS %.2f Carta 2\n\n", pib, pib2);
+
+    //Aqui será usado a estrutura IF e ELSE!
+    if ( popula > popula2 )
+    {
+        printf("A primeira carta venceu em população!!!\n");
+        pontos++;
+    } else
+    {
+        printf("A segunda carta venceu em população!!!\n");
+
+        pontos2++;
+    }
+
+    if ( pib > pib2 )
+    {
+        printf("A primeira carta venceu em PIB!!!\n");
+        pontos++;
+    } else
+    {
+        printf("A segunda carta venceu em PIB!!!\n");
+        pontos2++;
+    }
+
+    if ( superpoder1 > superpoder2 )
+    {
+        printf("A primeira carta venceu em Super Poder!!!\n");
+        pontos++;
+    } else
+    {
+        printf("A segunda carta venceu em Super Poder!!!\n");
+        pontos2++;
+    }
+
+    /*Aqui vou implementra uma questão de desempate entre as duas cartas utilizando os pontuadores que 
+    coloquei enquanto o programa verificava se as variáveis eram verdadeiras ou falsas*/
+
+    if (pontos > pontos2)
+    {
+        printf("\nParabéns, a cidade %s venceu!", cidade);
+    } else 
+    { 
+        printf("\nParabéns, a cidade %s venceu!", cidade2);
+    }
+
+return 0;   
 }
